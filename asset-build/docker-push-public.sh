2a8c93526a08e8f32365212644eb69f138af8753
ci-penbot-01 <a1.penbot@amd.com>
2025-08-21 06:42:10 +0530

    azure is not supported by rocm, we have removed it from build, asset push was missed (#798) (#800)

--- asset-build/docker-push-public.sh
+++ asset-build/docker-push-public.sh
@@ -8,8 +8,8 @@ fi
 
 PUBLIC_TAG=${PUBLIC_TAG:-latest}
 PRIVATE_TAG=${PRIVATE_TAG:-latest}
-AZ_PUBLIC_TAG="$PUBLIC_TAG-azl3"
-AZ_PRIVATE_TAG_TAG="$PRIVATE_TAG-azl3"
+#AZ_PUBLIC_TAG="$PUBLIC_TAG-azl3"
+#AZ_PRIVATE_TAG_TAG="$PRIVATE_TAG-azl3"
 
 docker rmi registry.test.pensando.io:5000/device-metrics-exporter/exporter:$PRIVATE_TAG
 docker pull registry.test.pensando.io:5000/device-metrics-exporter/exporter:$PRIVATE_TAG
@@ -19,9 +19,9 @@ docker rmi registry.test.pensando.io:5000/device-metrics-exporter-sriov/exporter
 docker pull registry.test.pensando.io:5000/device-metrics-exporter-sriov/exporter:$PRIVATE_TAG
 docker tag registry.test.pensando.io:5000/device-metrics-exporter-sriov/exporter:$PRIVATE_TAG amdpsdo/device-metrics-exporter-sriov:$PUBLIC_TAG
 
-docker rmi registry.test.pensando.io:5000/device-metrics-exporter/exporter:$AZ_PRIVATE_TAG
-docker pull registry.test.pensando.io:5000/device-metrics-exporter/exporter:$AZ_PRIVATE_TAG
-docker tag registry.test.pensando.io:5000/device-metrics-exporter/exporter:$AZ_PRIVATE_TAG amdpsdo/device-metrics-exporter:$AZ_PUBLIC_TAG
+#docker rmi registry.test.pensando.io:5000/device-metrics-exporter/exporter:$AZ_PRIVATE_TAG
+#docker pull registry.test.pensando.io:5000/device-metrics-exporter/exporter:$AZ_PRIVATE_TAG
+#docker tag registry.test.pensando.io:5000/device-metrics-exporter/exporter:$AZ_PRIVATE_TAG amdpsdo/device-metrics-exporter:$AZ_PUBLIC_TAG
 
 docker rmi registry.test.pensando.io:5000/test-runner/test-runner:$PRIVATE_TAG
 docker pull registry.test.pensando.io:5000/test-runner/test-runner:$PRIVATE_TAG
@@ -30,5 +30,5 @@ docker tag registry.test.pensando.io:5000/test-runner/test-runner:$PRIVATE_TAG a
 docker login --username=shreyajmeraamd --password-stdin <<< $DOCKERHUB_TOKEN
 docker push amdpsdo/device-metrics-exporter:$PUBLIC_TAG
 docker push amdpsdo/device-metrics-exporter-sriov:$PUBLIC_TAG
-docker push amdpsdo/device-metrics-exporter:$AZ_PUBLIC_TAG
+#docker push amdpsdo/device-metrics-exporter:$AZ_PUBLIC_TAG
 docker push amdpsdo/test-runner:$PUBLIC_TAG
--- asset-build/exporter-asset-push.sh
+++ asset-build/exporter-asset-push.sh
@@ -40,7 +40,7 @@ copy_artifacts () {
     # copy docker image ubi9.4
     cp /device-metrics-exporter/docker/device-metrics-exporter-sriov-latest.tar.gz $BUNDLE_DIR/device-metrics-exporter-sriov-latest-$RELEASE.tar.gz
     # copy docker image azure coreos 3
-    cp /device-metrics-exporter/docker/device-metrics-exporter-latest-azure.tar.gz $BUNDLE_DIR/device-metrics-exporter-latest-azure-$RELEASE.tar.gz
+    #cp /device-metrics-exporter/docker/device-metrics-exporter-latest-azure.tar.gz $BUNDLE_DIR/device-metrics-exporter-latest-azure-$RELEASE.tar.gz
     # copy docker mock image
     cp /device-metrics-exporter/docker/device-metrics-exporter-mock-latest.tar.gz $BUNDLE_DIR/device-metrics-exporter-mock-latest-$RELEASE.tar.gz
     # copy debian ubuntu packages
@@ -65,11 +65,11 @@ docker_push () {
     docker push $EXPORTER_IMAGE_URL:$tag
 
     # azurelinux3 image push
-    azuretag="$tag-azl3"
-    docker load -i /device-metrics-exporter/docker/device-metrics-exporter-latest-azure.tar.gz
-    docker inspect $EXPORTER_IMAGE_URL:latest | grep "HOURLY"
-    docker tag $EXPORTER_IMAGE_URL:latest $EXPORTER_IMAGE_URL:$azuretag
-    docker push $EXPORTER_IMAGE_URL:$azuretag
+    #azuretag="$tag-azl3"
+    #docker load -i /device-metrics-exporter/docker/device-metrics-exporter-latest-azure.tar.gz
+    #docker inspect $EXPORTER_IMAGE_URL:latest | grep "HOURLY"
+    #docker tag $EXPORTER_IMAGE_URL:latest $EXPORTER_IMAGE_URL:$azuretag
+    #docker push $EXPORTER_IMAGE_URL:$azuretag
 
     # rhel 9 sriov image push
     docker load -i /device-metrics-exporter/docker/device-metrics-exporter-latest.tar.gz
@@ -90,8 +90,8 @@ docker_push () {
       docker tag $EXPORTER_SRIOV_IMAGE_URL:$tag amdpsdo/device-metrics-exporter-sriov:$RELEASE
       docker push amdpsdo/device-metrics-exporter-sriov:$RELEASE
       # azure linux3
-      docker tag $EXPORTER_IMAGE_URL:$azuretag amdpsdo/device-metrics-exporter:$RELEASE-azl3
-      docker push amdpsdo/device-metrics-exporter:$RELEASE-azl3
+      #docker tag $EXPORTER_IMAGE_URL:$azuretag amdpsdo/device-metrics-exporter:$RELEASE-azl3
+      #docker push amdpsdo/device-metrics-exporter:$RELEASE-azl3
     fi
 }
 
