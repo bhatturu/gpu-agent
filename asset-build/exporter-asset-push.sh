0cb0891ce935c953186d0f1ba72641705008f6a9
Praveen Kumar Shanmugam <58961022+spraveenio@users.noreply.github.com>
2026-02-12 15:46:27 -0800

    debian artifacts to include release label for easy identification (#1091)
    minor typo for one of the artifact fixed

--- asset-build/exporter-asset-push.sh
+++ asset-build/exporter-asset-push.sh
@@ -50,16 +50,16 @@ copy_artifacts () {
     # copy docker mock image
     cp /device-metrics-exporter/docker/device-metrics-exporter-mock-latest.tar.gz $BUNDLE_DIR/device-metrics-exporter-mock-latest-$RELEASE.tar.gz
     # copy debian ubuntu packages
-    cp /device-metrics-exporter/bin/amdgpu-exporter_22.04_amd64.deb  $BUNDLE_DIR/amdgpu-exporter_${DEBIAN_VERSION}~22.04_amd64.deb
-    cp /device-metrics-exporter/bin/amdgpu-exporter_24.04_amd64.deb  $BUNDLE_DIR/amdgpu-exporter_${DEBIAN_VERSION}~24.04_amd64.deb
-    cp /device-metrics-exporter/bin/amdgpu-exporter-sriov_22.04_amd64.deb  $BUNDLE_DIR/amdgpu-exporter-sriov__${DEBIAN_VERSION}~22.04_amd64.deb
-    cp /device-metrics-exporter/bin/amdgpu-exporter-sriov_24.04_amd64.deb  $BUNDLE_DIR/amdgpu-exporter-sriov__${DEBIAN_VERSION}~24.04_amd64.deb
+    cp /device-metrics-exporter/bin/amdgpu-exporter_22.04_amd64.deb  $BUNDLE_DIR/amdgpu-exporter_$RELEASE~22.04_amd64.deb
+    cp /device-metrics-exporter/bin/amdgpu-exporter_24.04_amd64.deb  $BUNDLE_DIR/amdgpu-exporter_$RELEASE~24.04_amd64.deb
+    cp /device-metrics-exporter/bin/amdgpu-exporter-sriov_22.04_amd64.deb  $BUNDLE_DIR/amdgpu-exporter-sriov_$RELEASE~22.04_amd64.deb
+    cp /device-metrics-exporter/bin/amdgpu-exporter-sriov_24.04_amd64.deb  $BUNDLE_DIR/amdgpu-exporter-sriov_$RELEASE~24.04_amd64.deb
     # copy NIC debian ubuntu packages
-    cp /device-metrics-exporter/bin/amdnic-exporter_22.04_amd64.deb  $BUNDLE_DIR/amdnic-exporter_${DEBIAN_VERSION}~22.04_amd64.deb
-    cp /device-metrics-exporter/bin/amdnic-exporter_24.04_amd64.deb  $BUNDLE_DIR/amdnic-exporter_${DEBIAN_VERSION}~24.04_amd64.deb
+    cp /device-metrics-exporter/bin/amdnic-exporter_22.04_amd64.deb  $BUNDLE_DIR/amdnic-exporter_$RELEASE~22.04_amd64.deb
+    cp /device-metrics-exporter/bin/amdnic-exporter_24.04_amd64.deb  $BUNDLE_DIR/amdnic-exporter_$RELEASE~24.04_amd64.deb
     # copy rpm rhel9 packages
-    cp /device-metrics-exporter/bin/amdgpu-exporter-rhel9.x86_64.rpm $BUNDLE_DIR/amdgpu-exporter-${DEBIAN_VERSION}.x86_64.rpm
-    cp /device-metrics-exporter/bin/amdgpu-exporter-sriov-rhel9.x86_64.rpm $BUNDLE_DIR/amdgpu-exporter-sriov-${DEBIAN_VERSION}.x86_64.rpm
+    cp /device-metrics-exporter/bin/amdgpu-exporter-rhel9.x86_64.rpm $BUNDLE_DIR/amdgpu-exporter-$RELEASE.x86_64.rpm
+    cp /device-metrics-exporter/bin/amdgpu-exporter-sriov-rhel9.x86_64.rpm $BUNDLE_DIR/amdgpu-exporter-sriov-$RELEASE.x86_64.rpm
     # copy helm charts
     cp /device-metrics-exporter/helm-charts/device-metrics-exporter-charts.tgz $BUNDLE_DIR/device-metrics-exporter-charts-$RELEASE.tgz
     cp /device-metrics-exporter/helm-charts/nic-device-metrics-exporter-charts.tgz $BUNDLE_DIR/nic-device-metrics-exporter-charts-$RELEASE.tgz
